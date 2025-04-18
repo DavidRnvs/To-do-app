7b33994014a60a607ce2b065464d6858be625409
aro310 <aroratovoharison@gmail.com>
2025-04-19 00:22:09 +0300

    changement type date!

--- back/todo/tache/models.py
+++ back/todo/tache/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 import datetime
+from django.utils import timezone
+
+
+TACHE_DATE_MAX = timezone.make_aware(datetime.datetime(2025, 4, 19))
+TACHE_DATE_MIN = timezone.make_aware(datetime.datetime(2020, 1, 1))
 
 class User(models.Model):
     Username = models.fields.TextField()
@@ -13,14 +18,14 @@ class Tache(models.Model):
         
     date_de_creation = models.DateTimeField(
         validators=[
-            MaxValueValidator(limit_value=datetime.datetime(2025, 4, 19)),
-            MinValueValidator(limit_value=datetime.datetime(2025, 1, 1))
+            MaxValueValidator(limit_value=TACHE_DATE_MAX),
+            MinValueValidator(limit_value=TACHE_DATE_MIN)
         ]
     )
     date_de_fin = models.DateTimeField(
         validators=[
-            MaxValueValidator(limit_value=datetime.datetime(2025, 4, 19)),
-            MinValueValidator(limit_value=datetime.datetime(2025, 1, 1))
+            MaxValueValidator(limit_value=TACHE_DATE_MAX),
+            MinValueValidator(limit_value=TACHE_DATE_MIN)
         ]
     )
     nom_tache = models.fields.CharField(max_length=50)
